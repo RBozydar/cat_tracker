b20027cc313336955c1c04c4cb40c3b9496a0634
Robert Wasilewski <wasilewski.robert@gmail.com>
2024-12-01 21:13:37 +0100

    update CI, make history page dynamic

--- src/app/history/page.tsx
+++ src/app/history/page.tsx
@@ -2,6 +2,8 @@ import { Suspense } from 'react'
 import { prisma } from '@/lib/db'
 import { HistoryClient } from '@/components/history-client'
 
+export const dynamic = 'force-dynamic'
+
 export default async function HistoryPage() {
   const cats = await prisma.cat.findMany({
     include: {
